0100809006f75fd245ef4c7770f9c823b625ab74
cengi <cengizjan1457@gmail..com>
2024-10-13 12:37:38 +0300

    sdsd

---
+++ basic_locust_taskclass_neting.py
@@ -0,0 +1,33 @@
+from locust import User,TaskSet,task,between
+
+class UserBehaviour(TaskSet):
+
+   @task()
+   class Cart_Module(TaskSet):
+     @task()
+     def add_cart(self):
+        print("I am add to cart")
+
+     @task()
+     def delete_cart(self):
+       print("I am delete cart")
+
+     @task()
+     def stop(self):
+       print("I am stopping")
+       self.interrupt()
+   @task()
+   class Product_Module(TaskSet):
+     @task()
+     def view_product(self):
+      print("I am view product")
+
+     @task()
+     def add_product(self):
+        print("I am add product")
+        self.interrupt()
+
+class MyUser(User):
+    wait_time=between(1,2)
+    tasks=[UserBehaviour]
+
